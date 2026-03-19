ffa8ecee34d6b02d5062755fdb8dd291457e0eb0
yoons <yooons3749@gmail.com>
2026-03-19 17:45:29 +0900

    c++ first step

---
+++ C++_Learning/ch_01/NameSpace.h
@@ -0,0 +1,4 @@
+#pragma once
+namespace ns {
+	int  Num = 319;
+}
--- C++_Learning/ch_01/main.cpp
+++ C++_Learning/ch_01/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "NameSpace.h"
 
 int main() {
-	std::cout << "hey" << std::endl;
-	return 0;
+	std::cout <<  "hey" << ns::Num << std::endl;  // std = namespace , cout/endl = iostream의 stddp 저장
+	return 0;   // ex) std::cout << output << output
 }
+
+
+//This is first day of c++ 
+//Creating headerfile and use it on main.cpp

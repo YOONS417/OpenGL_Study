01fadc6585af7ff14f9c78e5c909a43cb40aec0a
yoons <yooons3749@gmail.com>
2026-04-03 19:37:41 +0900

    Start Shader project & use const for screen size

--- Projects/Shader/Shader.cpp
+++ Projects/Shader/Shader.cpp
@@ -5,30 +5,34 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void proccessInput(GLFWwindow* window);
 
+const unsigned int Screen_Wdith = 800;
+const unsigned int Screen_Height = 600;
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Project_Triangle", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(Screen_Wdith, Screen_Height, "Project_Triangle", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return  -1;
     }
-
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {   
         std::cout << " Failed to initialze GLAD" << std::endl;  
+        glfwTerminate();
+        return -1;
     }
 
     while (!glfwWindowShouldClose(window))
     {
         proccessInput(window);
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);   //BG Color
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
@@ -36,7 +40,7 @@ int main() {
     }
 
     glfwTerminate();
-    return 0;
+    return 0;   
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)

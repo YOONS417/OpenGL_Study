6aca0d33c256223366270b3ce0cf93ed0e3bcba8
yoons <yooons3749@gmail.com>
2026-08-27 23:33:04 +0900

    remove unnecessary shader in model loaging project

--- Projects/Model_Loading/MeshClass.cpp
+++ Projects/Model_Loading/MeshClass.cpp
@@ -1,2 +1,26 @@
+#include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>  
+#include <vector>
+#include "ShaderClass.h"
+
+using namespace std;
+
+struct Vertex {
+	glm::vec3 Position;  // 정점 위치
+	glm::vec3 Normal;	 // 법선	
+	glm::vec2 TexCoord;	 //텍스쳐 좌표	
+};
+
+struct Texture {
+	unsigned int id;    
+	string type;	//diffuse, specular등 타입
+};
+
+class Meah {
+	public:
+		vector<Vertex> vertices;
+		vector<unsigned int> indices;
+		vector<Texture> textures;
+
+}; 

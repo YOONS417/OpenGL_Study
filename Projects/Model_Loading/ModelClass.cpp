a2626a0ff61bd99301a91286b863d6221afd2d32
yoons <yooons3749@gmail.com>
2026-09-01 21:45:08 +0900

    add modelclass struct

--- Projects/Model_Loading/ModelClass.cpp
+++ Projects/Model_Loading/ModelClass.cpp
@@ -10,3 +10,53 @@
 #include <iostream>
 #include <vector>
 #include "MeshClass.h"
+
+using namespace std;
+
+// 텍스쳐 파일 로드용 함수
+unsigned  int TextureFromFile(const char* path, const std::string& direcroty);
+
+class Model
+{
+	public:
+		vector<Mesh> meshes;
+		string direcroty;
+
+		vector<Texture> loaded_texture;
+
+		Model(string const& path)
+		{
+			loadModel(path);
+		}
+
+		void Draw(Shader shader)
+		{
+			for (unsigned int i = 0; i < meshes.size(); i++)
+				meshes[i].Draw(shader);
+		}
+
+	private:
+		void loadModel(string const& path)
+		{
+			Assimp::Importer importer;
+
+			const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+		}
+
+		void processNode(aiNode* node, const aiScene* scene)
+		{
+
+		}
+
+		Mesh porcessMesh(aiMesh* mesh, const aiScene* scene)
+		{
+
+		}
+
+		vector<Texture> loadMaterialTextrues(aiMaterial* material, aiTextureType type, string typeName)
+		{
+			
+		}
+
+};

59a0e43677df432fc8b0f1165d8fba20e850e614
yoons <yooons3749@gmail.com>
2026-09-04 21:46:55 +0900

    push again

--- Projects/Model_Loading/ModelClass.h
+++ Projects/Model_Loading/ModelClass.h
@@ -164,7 +164,7 @@ class Model
 				{
 					Texture texture;
 					// 이미지를 읽어 GPU 메모리에 올리고 텍스쳐 ID 반환
-					texture.id = LoadTexture(string(str.C_Str()), this->directory, true);
+					texture.id = LoadTexture(string(str.C_Str()), true);
 					texture.type = typeName;
 					texture.path = str.C_Str();
 

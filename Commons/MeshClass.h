b8f9385645637415a14366ca2b3dbffb0dd8a44b
yoons <yooons3749@gmail.com>
2026-09-10 14:47:31 +0900

    remake depth testing project

---
+++ Commons/MeshClass.h
@@ -0,0 +1,108 @@
+#pragma once
+#ifndef MESH_S
+#define MESH_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>  
+#include <string>
+#include <vector>
+#include "ShaderClass.h"
+
+using namespace std;
+
+//각 객체들만 모은 동적 배열
+//데이터 보관 : 정점, 인덱스, 텍스쳐
+//GPU 버퍼 관리 : VAO, VBO, EBO 생성 및 바인딩
+struct Vertex {
+	glm::vec3 Position;  // 정점 위치
+	glm::vec3 Normal;	 // 법선	
+	glm::vec2 TexCoord;	 // 텍스쳐 좌표	
+};
+
+struct Texture {
+	unsigned int id;	// 텍스쳐 바인딩 ID   
+	string type;		// diffuse, specular등 타입
+	string path;		// 파일 경로(중복 로드 방지)
+};
+// 실질적으로 OpenGL GPU메모리에 접근할 수 있는 Mesh 객체	
+class Mesh {
+	public:
+		vector<Vertex> vertices;
+		vector<unsigned int> indices;
+		vector<Texture> textures;
+
+		unsigned int VAO;  //버퍼 객체
+
+		// 생성자 : 데이터 할당 및 GPU 버퍼 세팅
+		Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+		{
+			this->vertices = vertices;
+			this->indices = indices;
+			this->textures = textures;
+
+			setupMesh();
+		}
+		// Mesh를 그리기 위해 Shader에 텍스쳐 정보를 전달하고, VAO를 바인딩 후 glDrawElements() 호출
+		void Draw(Shader& shader)	
+		{
+			unsigned int diffuse_nr = 1;	// 텍스쳐 타입의 개수
+			unsigned int specular_nr = 1;
+
+			for (unsigned int i = 0; i < textures.size(); i++)  // 텍스쳐의 총 개수까지
+			{
+				glActiveTexture(GL_TEXTURE0 + i);	// 다음 텍스쳐 슬롯을 계속 활성화
+
+				// 하나의 메쉬에 여러 개의 diffuse나 specular가 포함될 수 있음
+				string number;
+				string name = textures[i].type;
+				// uniform이름을 동적으로 만듬 , ex) material.texture_diffuse1
+				if (name == "texture_diffuse")
+					number = to_string(diffuse_nr++);
+				else if (name == "texture_specular")
+					number = to_string(specular_nr++);
+				// shader의 material.texture_diffuse1 유니폼 변수가 몇 번째 텍스쳐 슬록을 참조하는지 알림
+				shader.setInt(("material." + name + number).c_str(), i); 
+				// GPU의 i번째 텍스쳐 슬롯에 텍스쳐 ID를 바인딩
+				glBindTexture(GL_TEXTURE_2D, textures[i].id);
+			}
+			// Mesh 그리기
+			glBindVertexArray(VAO);
+			glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+			// 상태 복원
+			glBindVertexArray(0);
+			glActiveTexture(GL_TEXTURE0);
+		}
+
+	private:
+		unsigned int VBO, EBO;
+		void setupMesh()
+		{
+			glGenBuffers(1, &VBO);	//GPU에 버퍼 객체 ID를 생성
+			glGenBuffers(1, &EBO);
+			glGenVertexArrays(1, &VAO);
+
+			glBindVertexArray(VAO);  // 바인딩 후 이후 설정(아래)되는 버퍼 상태를 기록
+
+			// VBO : Vertex 구조체 배열을 GPU VBO메모리에 복사
+			glBindBuffer(GL_ARRAY_BUFFER, VBO);	
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+			// EBO : 인덱스 데이터를 GPU EBO 메모리에 복사
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); 
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+			// 정점 위치
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+			// 각 면의 법선 
+			// 구조체는 멤버가 메모리에 연속적으로 배치 -> offsetof : Normal 변수가 몇 바이트 떨어져 있는지 자동로 계산
+			// (void*)(3 * sizeof(float)) - > (void*)offsetof(Vertex, Normal)
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+			// 텍스쳐 좌표
+			glEnableVertexAttribArray(2);
+			glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoord));
+
+			glBindVertexArray(0);	// VAO 바인딩 해제
+		}
+}; 
+#endif
---
+++ Projects/depthtesting/Texture.cpp
@@ -0,0 +1,51 @@
+#include <glad/glad.h>
+#include <iostream>
+#include <filesystem>
+#include "Texture.h"
+#include "stb_image.h"
+
+unsigned int LoadTexture(const std::string& path, const std::string& directory, bool flip) {
+	std::string fullpath;
+	if (directory.empty()) {	// directory가 빈 문자열("")인지 검사
+		fullpath = path;		// directory가 없음 -> path(~.png)만 사용
+	}
+	else {		// directory가 존재 -> dir + path 사용
+		fullpath = (std::filesystem::path(directory) / path).string();
+	}
+
+	unsigned int TextureID;
+	glGenTextures(1, &TextureID);
+
+	stbi_set_flip_vertically_on_load(flip); // 이미지 상하 반전 설정
+
+	int width, height, nrComponents;
+	// CPU 메모리로 이미지 로드
+	unsigned char* data = stbi_load(fullpath.c_str(), &width, &height, &nrComponents, 0);
+	if (data) {
+		GLenum format = GL_RGB;
+		if (nrComponents == 1) format = GL_RED;
+		else if (nrComponents == 3) format = GL_RGB;
+		else if (nrComponents == 4) format = GL_RGBA;
+
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // 텍스쳐 데이터 정렬 설정(1바이트 단위)
+		// GPU 메모리(VRAM)로 정송 및 바인딩
+		glBindTexture(GL_TEXTURE_2D, TextureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		// 텍스쳐 옵선 설정(repeat & filter) 
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+
+		std::cout << "Texture successfully loaded at path : " << fullpath << std::endl;
+		std::cout << "Texture Image nrComponents : " << nrComponents << std::endl;
+	}               
+	else {
+		std::cout << "Texture failed to load at path : " << fullpath << std::endl;
+		stbi_image_free(data);
+	}
+	return TextureID;
+}
